3281ce46a6400a51540657aa363afdf55bfd6068
j-flan <jhflanegan@gmail.com>
2020-01-08 14:35:18 -0700

    rocket binding

---
+++ Section 3/Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+    private void processInput(){
+        if (Input.GetKey(KeyCode.Space)){
+            print("space pressed");
+        }
+        if (Input.GetKey(KeyCode.A)){
+            print("rotate left");
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            print("rotate right");
+        }
+    }
+}
