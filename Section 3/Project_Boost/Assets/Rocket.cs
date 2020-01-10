21af5d5a2115406b21a571b9330dcaa46bae85e9
j-flan <jhflanegan@gmail.com>
2020-01-10 12:03:13 -0700

    thrust and sound

--- Section 3/Project_Boost/Assets/Rocket.cs
+++ Section 3/Project_Boost/Assets/Rocket.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidbody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -17,13 +20,19 @@ public class Rocket : MonoBehaviour
     }
     private void processInput(){
         if (Input.GetKey(KeyCode.Space)){
-            print("space pressed");
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying){ // no layering
+                audioSource.Play();
+            }      
+        }
+        else{
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A)){
-            print("rotate left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D)){
-            print("rotate right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
