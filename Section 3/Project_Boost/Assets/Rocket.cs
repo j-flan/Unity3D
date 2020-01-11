bbf9f06051db1b4149c63b88a515c6681e98c0eb
j-flan <jhflanegan@gmail.com>
2020-01-11 15:20:39 -0700

    particle effects & second level

--- Section 3/Project_Boost/Assets/Rocket.cs
+++ Section 3/Project_Boost/Assets/Rocket.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -8,32 +7,74 @@ public class Rocket : MonoBehaviour
     AudioSource audioSource;
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip explosion;
+    [SerializeField] AudioClip win;
+    [SerializeField] AudioClip loadLevel;
+
+    [SerializeField] ParticleSystem thrustParticle;
+    [SerializeField] ParticleSystem explodeParticle;
+    [SerializeField] ParticleSystem winParticle;
+
+
+    enum State {alive, dead, transition};
+    State state = State.alive;
+    int currentLevel = 0;
+
     // Start is called before the first frame update
     void Start()
-    {
+    {       
         rigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        thrust();
-        rotate();
+    void Update(){
+        if (state == State.alive){
+            thrustResponse();
+            rotateResponse();
+        }
     }
     void OnCollisionEnter(Collision collision){
+
+        if (state != State.alive)
+            return;
+
         switch(collision.gameObject.tag){
             case "friendly":
-                print("thats fine...");
+                print("friend!");
+                audioSource.PlayOneShot(loadLevel);
                 break;
-            case "dead":
-                print("dead");
+
+            case "Finish":
+                winSequence();
+                break;
+                
+            default:
+                deathSequence();
                 break;
         }
 
     }
-    private void rotate(){
-        
+    private void deathSequence(){
+        state = State.dead;
+        audioSource.Stop();
+        audioSource.PlayOneShot(explosion);
+        explodeParticle.Play();
+        Invoke("loadNextScene", 2f); //delay
+    }
+    private void winSequence(){
+        state = State.transition;
+        this.currentLevel += 1;
+        audioSource.Stop();
+        audioSource.PlayOneShot(win);
+        winParticle.Play();
+        Invoke("loadNextScene", 1f); //delay
+    }
+    private void loadNextScene(){
+        SceneManager.LoadScene(this.currentLevel);
+    }
+    private void rotateResponse(){
         rigidbody.freezeRotation = true; // manual control
         float rotationFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A)){
@@ -45,15 +86,20 @@ public class Rocket : MonoBehaviour
         }
         rigidbody.freezeRotation = false; //relase user physics
     }
-    private void thrust(){
+    private void thrustResponse(){
         if (Input.GetKey(KeyCode.Space)){
-            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!audioSource.isPlaying){ // no layering
-                audioSource.Play();
-            }      
+             applyThrust();
         }
         else{
             audioSource.Stop();
-        }
+            thrustParticle.Stop();
+        }     
+    }
+    private void applyThrust(){
+        rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying){ // no layering
+                audioSource.PlayOneShot(mainEngine);
+                thrustParticle.Play();
+            }     
     }
 }
